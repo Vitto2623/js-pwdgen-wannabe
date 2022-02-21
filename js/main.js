2946b04e9a33aabc4ca768ddd0c22ec324e8019c
Vitto2623 <vtr.1@icloud.com>
2022-02-21 14:46:56 +0100

    Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Nome utente 
+const nome = prompt("Inserisca il suo nome")
+document.getElementById('name').innerHTML= nome;
+// Cognome utente
+const cognome = prompt("Inserisca il suo cognome")
+document.getElementById('surname').innerHTML= cognome;
+// Colore preferito
+const colorePreferito = prompt("Inserisca il suo colore preferito")
+document.getElementById('favcolor').innerHTML= colorePreferito;
+// Testo pagina
+document.writeln( '<h1>nomeCognomeColorePreferito21</h1>');
